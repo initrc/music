28fa30c04510fc41c65264cb9816b416dc65eebb
David Shi <DavidSnooker@gmail.com>
2015-12-29 14:42:00 -0800

    Test ng-route

--- js/music.js
+++ js/music.js
@@ -1,10 +1,26 @@
-angular.module('musicApp', [])
-  .controller('PlaylistController', function() {
-    var playlist = this;
-    playlist.songs = [
-      {name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15'},
-      {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
-    ];
-  });
+var app = angular.module('musicApp', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+  $log.info($routeParams);
+  $routeProvider.when('/:id', {
+    templateUrl: 'index.html',
+    controller: 'PlaylistController'
+  })
+}]);
+app.controller('PlaylistController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  songs = [
+    {id: '201507', name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
+    {id: '201502', name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15'},
+  ];
+  var id = $routeParams.id;
+  var playingIndex = 0;
+  for (i = 0; i < songs.len; i++) {
+    if (songs[i].id == id) {
+      playingIndex = i;
+      break;
+    }
+  }
+  $scope.songs = songs;
+  $scope.playingIndex = playingIndex;
+}]);
 
 
