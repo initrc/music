bd474a54c11430739c7a227e3023dbb4540014d6
David Shi <DavidSnooker@gmail.com>
2015-12-29 14:50:53 -0800

    Remove ng-route

--- js/music.js
+++ js/music.js
@@ -1,26 +1,7 @@
-var app = angular.module('musicApp', ['ngRoute']);
-app.config(['$routeProvider', function($routeProvider) {
-  $log.info($routeParams);
-  $routeProvider.when('/:id', {
-    templateUrl: 'index.html',
-    controller: 'PlaylistController'
-  })
-}]);
-app.controller('PlaylistController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  songs = [
+var app = angular.module('musicApp', []);
+app.controller('PlaylistController', ['$scope', function($scope) {
+  $scope.songs = [
     {id: '201507', name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
     {id: '201502', name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15'},
   ];
-  var id = $routeParams.id;
-  var playingIndex = 0;
-  for (i = 0; i < songs.len; i++) {
-    if (songs[i].id == id) {
-      playingIndex = i;
-      break;
-    }
-  }
-  $scope.songs = songs;
-  $scope.playingIndex = playingIndex;
 }]);
-
-
