2ffc8f380696e2dd5e8324eef52522a2ec8f6a99
David Shi <DavidSnooker@gmail.com>
2015-12-29 16:48:21 -0800

    Add all songs and better file structure

--- js/music.js
+++ js/music.js
@@ -6,13 +6,18 @@ app.config(function($sceDelegateProvider) {
 
 app.controller('PlaylistController', function($scope) {
   $scope.songs = [
-    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
-    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
+    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15', track: '07'},
+    {name: 'See You Again', artist: 'David Shi', album: 'Piano Covers \'15', track: '06'},
+    {name: 'The Place Where Wishes Come True', artist: 'David Shi', album: 'Piano Covers \'15', track: '05'},
+    {name: '回梦游仙', artist: 'David Shi', album: 'Piano Covers \'15', track: '04'},
+    {name: 'Porcelain', artist: 'David Shi', album: 'Piano Covers \'15', track: '03'},
+    {name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15', track: '02'},
+    {name: 'Sadness and Sorrow', artist: 'David Shi', album: 'Piano Covers \'15', track: '01'},
   ];
   $scope.getSongUrl = function(song) {
-    return song.name + '.mp3';
+    return song.album + '/' + song.track + ' ' + song.name + '.mp3';
   };
   $scope.getAlbumUrl = function(song) {
-    return 'image/' + song.album + '.jpg';
+    return song.album + '/cover.jpg';
   };
 });
