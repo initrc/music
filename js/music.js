6427703d36740f38a1320100cd60cc9bb4903290
David Shi <DavidSnooker@gmail.com>
2015-12-29 21:44:42 -0800

    Use local angular.min.js and add song desc

--- js/music.js
+++ js/music.js
@@ -2,13 +2,13 @@ var app = angular.module('musicApp', []);
 
 app.controller('PlaylistController', function($scope) {
   $scope.songs = [
-    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15', track: '07'},
-    {name: 'See You Again', artist: 'David Shi', album: 'Piano Covers \'15', track: '06'},
-    {name: 'The Place Where Wishes Come True', artist: 'David Shi', album: 'Piano Covers \'15', track: '05'},
-    {name: '回梦游仙', artist: 'David Shi', album: 'Piano Covers \'15', track: '04'},
-    {name: 'Porcelain', artist: 'David Shi', album: 'Piano Covers \'15', track: '03'},
-    {name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15', track: '02'},
-    {name: 'Sadness and Sorrow', artist: 'David Shi', album: 'Piano Covers \'15', track: '01'},
+    {name: '红颜旧', desc: '琅琊榜', artist: 'David Shi', album: 'Piano Covers \'15', track: '07'},
+    {name: 'See You Again', desc: 'Furious 7', artist: 'David Shi', album: 'Piano Covers \'15', track: '06'},
+    {name: 'The Place Where Wishes Come True', desc: 'Clannad', artist: 'David Shi', album: 'Piano Covers \'15', track: '05'},
+    {name: '回梦游仙', desc: '仙剑奇侠传四', artist: 'David Shi', album: 'Piano Covers \'15', track: '04'},
+    {name: 'Porcelain', desc: 'Helen Jane Long', artist: 'David Shi', album: 'Piano Covers \'15', track: '03'},
+    {name: 'Secret Base', desc: 'Anohana', artist: 'David Shi', album: 'Piano Covers \'15', track: '02'},
+    {name: 'Sadness and Sorrow', desc: 'Naruto', artist: 'David Shi', album: 'Piano Covers \'15', track: '01'},
   ];
   for (var i = 0; i < $scope.songs.length; i++) {
     var song = $scope.songs[i];
