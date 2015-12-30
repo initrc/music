03e1796c24870e2467092f223d3ad5a757b27e7e
David Shi <DavidSnooker@gmail.com>
2015-12-29 16:34:50 -0800

    Playlist works with sce delegate

--- js/music.js
+++ js/music.js
@@ -1,7 +1,18 @@
 var app = angular.module('musicApp', []);
-app.controller('PlaylistController', ['$scope', function($scope) {
+
+app.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist(['self'])
+});
+
+app.controller('PlaylistController', function($scope) {
   $scope.songs = [
-    {id: '201507', name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
-    {id: '201502', name: 'Secret Base', artist: 'David Shi', album: 'Piano Covers \'15'},
+    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
+    {name: '红颜旧', artist: 'David Shi', album: 'Piano Covers \'15'},
   ];
-}]);
+  $scope.getSongUrl = function(song) {
+    return song.name + '.mp3';
+  };
+  $scope.getAlbumUrl = function(song) {
+    return 'image/' + song.album + '.jpg';
+  };
+});
